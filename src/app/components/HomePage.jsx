1b77e31188b33bc654e42033f36146c635b11c3c
Tanishka Agarwal <144484154+Tanishka0067@users.noreply.github.com>
2024-09-26 18:50:53 +0530

    sidebar

--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -1,41 +1,66 @@
-import React from 'react'
-import img from './image.png'
+import React,{useState} from "react";
+import img from "./image.png";
 import { BiMenuAltLeft } from "react-icons/bi";
-import Image from 'next/image'
+import Image from "next/image";
+import Sidebar from "./Sidebar";
 const HomePage = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
-   <div>
-    <div className=' text-white text-6xl p-2 fixed'><button><BiMenuAltLeft /></button>
-    </div>
-    <div className='p-20 text-white flex flex-col items-center'>
-     <div className='flex flex-row justify-center gap-12 text-lg font-semibold'>
-      <div>HOW IT WORKS</div>
-      <div>DONATE</div>
+    <div>
+      <div className=" p-2 fixed">
+        <button
+          className=" text-white text-6xl"
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+
+        >
+          <BiMenuAltLeft />
+        </button>
+        {isSidebarOpen && <Sidebar />}
       </div>
-
-      <div>
-        <h1 className='text-4xl font-bold text-center p-4'>
-        Vocal Remover and Isolation
-        </h1>
-        <h3 className='text-center text-lg'>Separate voice from music out of a song free with powerful AI algorithms
-        </h3>
-        <Image src={img} alt="" className='w-9/12 p-6 mx-auto '/>
+      <div className="p-20 text-white flex flex-col items-center">
+        <div className="flex flex-row justify-center gap-12 text-lg font-semibold">
+          <div>HOW IT WORKS</div>
+          <div>DONATE</div>
         </div>
-        <div><button className='border-indigo-700 border-2 p-3 rounded-2xl'>Browse my files</button></div> 
-        <div className=' mt-40'>
-<h1 className='text-5xl font-medium hover:bg-indigo-500 p-4 '>Remove vocals from a song
-</h1>
-<p className='bg-gray-800/50 border-l-4 border-indigo-500 p-4 px-8 text-2xl mt-4'>This free online application will help remove vocals from a song by creating karaoke.
-<br/><br/>
 
-Once you choose a song, artificial intelligence will separate the vocals from the instrumental ones. You will get two tracks - a karaoke version of your song (no vocals) and acapella version (isolated vocals).
-<br/><br/>
-
-Despite the complexity and high cost of service, you can use it absolutely free. Processing usually takes about 10 seconds.</p>
+        <div>
+          <h1 className="text-4xl font-bold text-center p-4">
+            Vocal Remover and Isolation
+          </h1>
+          <h3 className="text-center text-lg">
+            Separate voice from music out of a song free with powerful AI
+            algorithms
+          </h3>
+          <Image src={img} alt="" className="w-9/12 p-6 mx-auto " />
         </div>
+        <div>
+          <button className="border-indigo-700 border-2 p-3 rounded-2xl">
+            Browse my files
+          </button>
+        </div>
+        <div className=" mt-40">
+          <h1 className="text-5xl font-medium hover:bg-indigo-500 p-4 ">
+            Remove vocals from a song
+          </h1>
+          <p className="bg-gray-800/50 border-l-4 border-indigo-500 p-4 px-8 text-2xl mt-4">
+            This free online application will help remove vocals from a song by
+            creating karaoke.
+            <br />
+            <br />
+            Once you choose a song, artificial intelligence will separate the
+            vocals from the instrumental ones. You will get two tracks - a
+            karaoke version of your song (no vocals) and acapella version
+            (isolated vocals).
+            <br />
+            <br />
+            Despite the complexity and high cost of service, you can use it
+            absolutely free. Processing usually takes about 10 seconds.
+          </p>
+        </div>
+      </div>
     </div>
-   </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,27 +1,78 @@
-// src/app/components/Sidebar.jsx
-"use client";
-
-import { Box, NavLink, Stack } from '@mantine/core';
-import { useRouter } from 'next/navigation';
-
+import { useRouter } from "next/navigation"; 
+import { TbPrismLight } from "react-icons/tb";
+import { FaMicrophone } from "react-icons/fa6";
+import { CgArrowsMergeAltH } from "react-icons/cg";
+import { PiArrowsMerge } from "react-icons/pi";
+import { RiScissors2Line } from "react-icons/ri";
+import { HiOutlineDotsCircleHorizontal } from "react-icons/hi";
+import { TbTriangleOff } from "react-icons/tb";
+import { BsFillRecordCircleFill } from "react-icons/bs";
 const Sidebar = () => {
   const router = useRouter();
-
   return (
-    <Box
-      sx={{
-        width: 250,
-        height: '100vh',
-        backgroundColor: '#f4f4f5',
-        padding: '20px',
-      }}
-    >
-      <Stack spacing="md">
-        <NavLink label="Audio Cutter" onClick={() => router.push('/audio-cutter')} />
-        {/* Add more links here if needed */}
-        <NavLink label="Other Feature" onClick={() => router.push('/other-feature')} />
-      </Stack>
-    </Box>
+    <>
+      <div className="font-thin text-gray-400 text-xl p-2 flex flex-col gap-5 items-center justify-center ">
+        <a href="">
+          <div className="items-center flex flex-col gap-2">
+            {" "}
+            <HiOutlineDotsCircleHorizontal />
+            <p className="text-xs">Remover</p>
+          </div>
+        </a>{" "}
+        <a href="">
+          <div className="items-center flex flex-col gap-2" x>
+            <TbPrismLight />
+            <p className="text-xs">Splitter</p>
+          </div>
+        </a>
+        <a href="">
+          <div className="items-center flex flex-col gap-2">
+            {" "}
+            <PiArrowsMerge />
+            <p className="text-xs">Pitch</p>
+          </div>
+        </a>
+        <a href="">
+          <div className="items-center flex flex-col gap-2">
+            <TbTriangleOff />
+            <p className="text-xs">
+              Key BPM <br /> Finder
+            </p>
+          </div>
+        </a>
+        <a href=""
+        onClick={(e) => {
+          e.preventDefault(); 
+          router.push("/audio-cutter"); 
+        }}>
+          <div className="items-center flex flex-col gap-2">
+            {" "}
+            <RiScissors2Line />
+            <p className="text-xs">Cutter</p>
+          </div>
+        </a>
+        <a href="">
+          <div className="items-center flex flex-col gap-2">
+            {" "}
+            <CgArrowsMergeAltH />
+            <p className="text-xs">Joiner</p>
+          </div>
+        </a>
+        <a href="">
+          <div className="items-center flex flex-col gap-2">
+            {" "}
+            <FaMicrophone />
+            <p className="text-xs">Recorder</p>
+          </div>
+        </a>
+        <a href="">
+          <div className="items-center flex flex-col gap-2">
+            <BsFillRecordCircleFill />
+            <p className="text-xs">Karaoke</p>
+          </div>
+        </a>
+      </div>
+    </>
   );
 };
 
